85abd924a647e35eed8ee456fd5f0c6f94beab04
pwruoya <pruoya@kabarak.ac.ke>
2024-04-08 23:18:24 +0300

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim list1 As ListViewItem
+        list1 = Me.ListView1.Items.Add(Me.TextBox1.Text)
+        list1.SubItems.Add(Me.TextBox2.Text)
+        list1.SubItems.Add(Me.TextBox3.Text)
+        list1.SubItems.Add(Me.TextBox4.Text)
+        list1.SubItems.Add(Me.TextBox5.Text)
+        clear()
+    End Sub
+    Public Function clear()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+
+    End Function
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+        If ListView1.SelectedItems.Count > 0 Then
+            TextBox1.Text = ListView1.SelectedItems(0).SubItems(0).Text
+            TextBox2.Text = ListView1.SelectedItems(0).SubItems(1).Text
+            TextBox3.Text = ListView1.SelectedItems(0).SubItems(2).Text
+            TextBox4.Text = ListView1.SelectedItems(0).SubItems(3).Text
+            TextBox5.Text = ListView1.SelectedItems(0).SubItems(4).Text
+        End If
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("form2update")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("form2update")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4ea151cc-16b9-4aff-8205-3a0410b76def")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
